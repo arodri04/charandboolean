183a0e1fe1a3dc43a0c5ea64bc44267b39dcc167
Sam Rodriguez <augustine.rodriguez55@gmail.com>
2020-07-08 01:11:20 -0700

    added examples

---
+++ charboolean/src/acadamy/learnprogramming/Main.java
@@ -0,0 +1,24 @@
+package acadamy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044'; //  is how you do a unicode char
+        char copyChar = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(copyChar);
+
+        // booleans - true or false
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+
+        boolean isPersonOver21 = true;
+
+    }
+}
